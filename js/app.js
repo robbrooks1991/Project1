28c80edcc017fd07f7c74afc9377cde05a189c83
Robert Brooks <brooksr144@gmail.com>
2017-06-27 19:59:19 +0100

    Angular Updated and working for page navigation

--- js/app.js
+++ js/app.js
@@ -1,28 +1,26 @@
 angular.module('techno', ['ngRoute', 'RouteControllers']);
 
 angular.module('techno').config(function($locationProvider, $routeProvider) {
-    $locationProvider.html5mode([true]);
+    $locationProvider.html5Mode(true);  // Enable href routing without hashes
 
-    $routeProvider
-        .when("/", {
-            templateUrl: 'templates/home.html',
-            controller: 'HomeController'
+    $routeProvider.when('/', {
+        templateUrl: 'templates/home.html',
+        controller: 'HomeController'
+    })
+        .when('/listofdjs', {
+            templateUrl: 'templates/listofdjs.html',
+            controller: 'DJController'
         })
-
-    .when("/account", {
-        templateUrl: 'templates/account.html'
-    })
-
-    .when("/gallery", {
-        templateUrl: 'templates/gallery.html'
-    })
-
-    .when("/listen", {
-        templateUrl: 'templates/listen.html'
-    })
-
-    .otherwise({ //if a random website is entered it will return to the homepage.
-        redirectTo: '/'
-    });
-
+    .when('/listen', {
+            templateUrl: 'templates/listen.html',
+            controller: 'ListenController'
+        })
+    .when('/gallery', {
+            templateUrl: 'templates/gallery.html',
+            controller: 'GalleryController'
+        })
+    .when('/account', {
+            templateUrl: 'templates/account.html',
+            controller: 'AccountController'
+        })
 });
